33ddc8b19399bc60ab79664860873b55f3e63117
coque <coquelet.samuel@gmail.com>
2022-02-25 17:03:19 +0100

    pas fini...

--- database/passwordGen.php
+++ database/passwordGen.php
@@ -1,11 +1,23 @@
 <?php
 
-$password_en_clair = "Entrez umot de passe dans cette chaine";
+$password_en_clair = "Naruto1990";
+
 
 // Affichage du mot de passe crypté.
 $encrypted = password_hash($password_en_clair, PASSWORD_BCRYPT);
+echo $encrypted;
 
-// Exemple de véric-fication d'un mot de passe.
+// Exemple de vérification d'un mot de passe.
 if(password_verify($password_en_clair, $encrypted)) {
     // Les mots de passe correspondent, on peut par exemple enregistrer toutes les informations de l'utilisateur en session.
+    echo "<br>Les mots de passes correspondent<br>";
 }
+
+//mdp de tsunade
+$password_en_clair = "Samuel1990";
+
+// Affichage du mot de passe crypté.
+$encrypted = password_hash($password_en_clair, PASSWORD_BCRYPT);
+echo $encrypted;
+echo "<br>";
+
--- lib/functions.php
+++ lib/functions.php
@@ -2,13 +2,13 @@
 
 function issetPostParams(string ...$params): bool {
 foreach ($params as $param) {
-if (!isset($_POST[$param])) {
+    if (!isset($_POST[$param])) {
 // Si un seul paramètres donnés n'est pas là, alors je retourne false.
-return false;
-}
+        return false;
+    }
 }
 //Si on arrive jusqu'ici, ca signifie que tous les paramètres sont présents.
-return true;
+    return true;
 }
 
 /**
@@ -18,12 +18,12 @@ return true;
 */
 function sanitize($data) {
 // Supprime les espaces superflus en début et fin de chaine.
-$data = trim($data);
+    $data = trim($data);
 // Supprime les antislashes que les hackers pourraient utiliser pour échapper des caractères spéciaux.
-$data = stripslashes($data);
+    $data = stripslashes($data);
 // Transforme certains caractères spéciaux en entités HTML pour les rendre innofensifs.
-$data = htmlspecialchars($data);
+    $data = htmlspecialchars($data);
 // Ajoute des slashes pour éviter de fermer les chaînes de caractères dans le formulaire.
-$data = addslashes($data);
-return $data;
+    $data = addslashes($data);
+    return $data;
 }
